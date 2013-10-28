aaa51c4e863d74c304a78f3d1b9dcf88ab6b2962
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2013-10-27 23:54:34 -0600

    Add full implementation of YVBKeyLogger
    As it is right now the KeyLogger will save to the database all the
    KeyDown events with the current timestamp.

--- Keystats/AppDelegate.m
+++ Keystats/AppDelegate.m
@@ -22,23 +22,26 @@
 			// Opening and closing a database connection everytime a key is
 			// is fairly resource consuming, specially if you type kind of fast
 			NSString *databaseFilePath = [[NSBundle mainBundle] pathForResource:@"keystrokes" ofType:@""];
+			NSString *sqlInsert = nil;
+			NSDateFormatter *dateFormat = nil;
+			NSString *dateString = nil;
+
+			dateFormat = [[NSDateFormatter alloc] init];
+			[dateFormat setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+			dateString = [dateFormat stringFromDate:[NSDate date]];
+
 			FMDatabase *db = [FMDatabase databaseWithPath:databaseFilePath];
 			if (![db open]) {
 				NSLog(@"Could not successfully open the database");
 				return;
 			}
 			else{
-				NSString *something = [NSString stringWithFormat:@"\
-									   INSERT INTO\
-									   keystrokes (timestamp, type, keycode, ascii)\
-									   VALUES(null, %d, %llu, '%@');\
-									   commit;", eventType, keyCode, string];
-				if ([db executeUpdate:something]) {
-					return;
+				// SQL insert
+				sqlInsert = [NSString stringWithFormat:@"INSERT INTO keystrokes (timestamp, type, keycode, ascii) VALUES('%@', %d, %llu, '%@'); commit;", dateString, eventType, keyCode, string];
+				if (![db executeUpdate:sqlInsert]) {
+					NSLog(@"Unexpected error %@ FAILED!", sqlInsert);
 				}
-				else{
-					[db close];
-				}
+				[db close];
 			}
 		}
 	}];
