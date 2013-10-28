a00384e6c51a20a4e0c65915720e4a04a92f107a
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2013-10-27 21:45:16 -0600

    Add changes that implement a barebones keylogger
    This keylogger starts a program and starts dumping out all of the
    information to standard output.

--- Keystats/AppDelegate.m
+++ Keystats/AppDelegate.m
@@ -8,13 +8,42 @@
 
 #import "AppDelegate.h"
 
+#import "YVBKeyLogger.h"
+#import "FMDatabase.h"
+
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification{
 	// Insert code here to initialize your application
+
 	NSString *databaseFilePath = [[NSBundle mainBundle] pathForResource:@"keystrokes" ofType:@""];
 	NSLog(@"The database is at: %@", databaseFilePath);
 
+	NSDictionary *options = @{(__bridge id)kAXTrustedCheckOptionPrompt: @YES};
+	BOOL accessibilityEnabled = AXIsProcessTrustedWithOptions((__bridge CFDictionaryRef)options);
+
+	NSLog(@"Something happpend: %@", accessibilityEnabled ? @"bueno" : @"malo");
+
+//	YVBKeyLogger *someKeyLogger = [[YVKeyLogger alloc] initWithKeyPressedHandler:^(NSString *string, long long keyCode){
+//		NSLog(@"XXXXXX %@", string);
+//	}];
+	YVBKeyLogger *someKeyLogger = [[YVBKeyLogger alloc] init];
+	[someKeyLogger startLogging];
+
+	NSLog(@"The key logger %@ running", [someKeyLogger isLogging] ? @"is" : @"is not");
+
+//	FMDatabase *db = [FMDatabase databaseWithPath:databaseFilePath];
+//	if (![db open]) {
+//		NSLog(@"Could not successfully open the database");
+//		return;
+//	}
+//	NSLog(@"Database successfully open ...");
+//
+//	FMResultSet *s = [db executeQuery:@"SELECT * FROM keystrokesaaaa;"];
+//	while ([s next]) {
+//		//retrieve values for each record
+//		NSLog(@"Retrieving a result");
+//	}
 }
 
 @end
