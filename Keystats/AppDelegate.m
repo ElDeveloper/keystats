db2dd1497b2f3fddc9bbbad666e28d415cd5fa8f
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2013-10-31 01:34:39 -0600

    Add usage of YVBKeystrokesDataManager

--- Keystats/AppDelegate.m
+++ Keystats/AppDelegate.m
@@ -10,6 +10,7 @@
 
 #import "YVBKeyLogger.h"
 #import "FMDatabase.h"
+#import "YVBKeystrokesDataManager.h"
 
 @implementation AppDelegate
 
@@ -17,51 +18,28 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification{
 	// Insert code here to initialize your application
-	[totalCountLabel setStringValue:@"0"];
+	NSLog(@"The key logger %@ work as expected", [YVBKeyLogger requestEnableAccessibility] ? @"will" : @"will not");
+	NSString *databaseFilePath = [[NSBundle mainBundle] pathForResource:@"keystrokes" ofType:@""];
+
+	YVBKeystrokesDataManager * __block dataManager = [[YVBKeystrokesDataManager alloc] initWithFilePath:databaseFilePath];
+	[totalCountLabel setStringValue:[dataManager getTotalCount]];
 	[todayCountLabel setStringValue:@"0"];
 	[thisWeekCountLabel setStringValue:@"0"];
 	[thisMonthCountLabel setStringValue:@"0"];
 
-	NSLog(@"The key logger %@ work as expected", [YVBKeyLogger requestEnableAccessibility] ? @"will" : @"will not");
-
-	NSString * __block databaseFilePath = [[NSBundle mainBundle] pathForResource:@"keystrokes" ofType:@""];
 	NSDateFormatter * __block dateFormat = [[NSDateFormatter alloc] init];;
 	[dateFormat setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
-
-	FMDatabase * __block db = [FMDatabase databaseWithPath:databaseFilePath];
-	if (![db open]) {
-		NSLog(@"Could not successfully open the database");
-		return;
-	}
 
 	NSLog(@"The database filepath is %@", databaseFilePath);
 
 	YVBKeyPressed handlerBlock = ^(NSString *string, long long keyCode, CGEventType eventType){
 		if (eventType == kCGEventKeyDown) {
-			// Opening and closing a database connection everytime a key is
-			// is fairly resource consuming, specially if you type kind of fast
-			NSString *sqlInsert = nil;
 			NSString *dateString = nil;
 
-			// get the current time-stamp for this key
+			// get the current time-stamp for this keystroke
 			dateString = [dateFormat stringFromDate:[NSDate date]];
-
-			if ([db goodConnection]) {
-				// SQL insert
-				sqlInsert = [NSString stringWithFormat:@"INSERT INTO keystrokes (timestamp, type, keycode, ascii) VALUES('%@', %d, %llu, '%@'); commit;", dateString, eventType, keyCode, string];
-				if (![db executeUpdate:sqlInsert]) {
-					NSLog(@"Unexpected error %@ FAILED!", sqlInsert);
-				}
-				FMResultSet *countTotalResult = [db executeQuery:@"SELECT COUNT(*) FROM keystrokes;"];
-				if ([countTotalResult next]) {
-					[totalCountLabel setStringValue:[countTotalResult stringForColumnIndex:0]];
-				}
-				[countTotalResult close];
-			}
-			else{
-				NSLog(@"The connection was interruped, trying to reconnect ...");
-				[db open];
-			}
+			[dataManager addKeystrokeWithTimeStamp:dateString string:string keycode:keyCode andEventType:eventType];
+			[totalCountLabel setStringValue:[dataManager getTotalCount]];
 		}
 	};
 
