6f371738c77b1da80bb271a6e7f9bf7b8b549dab
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2013-12-24 12:09:20 -0700

    Fix indentation

--- KeyLogger/YVBKeystrokesDataManager.m
+++ KeyLogger/YVBKeystrokesDataManager.m
@@ -62,10 +62,11 @@
 -(BOOL)addKeystrokeWithTimeStamp:(NSString *)timestamp string:(NSString *)stringValue keycode:(long long)keyCode andEventType:(CGEventType)eventType{
 	// SQL insert
 	NSString *sqlInsert = [NSString stringWithFormat:@"INSERT INTO keystrokes (timestamp, type, keycode, ascii) VALUES('%@', %d, %llu, '%@'); commit;", timestamp, eventType, keyCode, stringValue];
-		if (![_database executeUpdate:sqlInsert]) {
-			NSLog(@"Unexpected error %@ FAILED!", sqlInsert);
-			return NO;
-		}
+
+	if (![_database executeUpdate:sqlInsert]) {
+		NSLog(@"Unexpected error %@ FAILED!", sqlInsert);
+		return NO;
+	}
 	return YES;
 }
 
