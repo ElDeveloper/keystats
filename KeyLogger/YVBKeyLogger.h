43855e3c977d72583e7f6b6c1c8e9e1b246042cc
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2013-12-24 12:05:58 -0700

    Fix typo

--- KeyLogger/YVBKeyLogger.h
+++ KeyLogger/YVBKeyLogger.h
@@ -3,7 +3,7 @@
 //  Keystats
 //
 //  Created by Yoshiki Vázquez Baeza on 10/27/13.
-//  Basede in code from J. Koivisto S.A.C. found in OSXKeyLogger
+//  Based in code from J. Koivisto S.A.C. found in OSXKeyLogger
 //  https://code.google.com/p/osxkeylogger
 //
 //  Copyright (c) 2013 Yoshiki Vázquez Baeza. All rights reserved.
